82e086b51b91032f8eebd8d9dd21f1ac96b67697
Đỗ Công Phước Anh <docongphuocanh.2697@gmail.com>
2020-09-21 23:43:35 +0700

    delete project

--- src/assets/avatar/index.js
+++
@@ -1,6 +0,0 @@
-import avtOnce from './Avatar1.png';
-import avtTow from './Avatar2.png';
-import avtThree from './Avatar3.png';
-import girl from './AvatarGirl.png';
-
-export { avtOnce, avtTow, avtThree, girl };
